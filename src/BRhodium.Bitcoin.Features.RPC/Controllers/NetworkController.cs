f6125e056ec17e6afe63b7c38974d561e1758cc0
BitCoin Rhodium <customers@bitcoinrh.org>
2018-08-27 13:24:44 +0200

    Clean repo, Two new RPC method DiconnedNode, ListBanned

--- src/BRhodium.Bitcoin.Features.RPC/Controllers/NetworkController.cs
+++ src/BRhodium.Bitcoin.Features.RPC/Controllers/NetworkController.cs
@@ -15,6 +15,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using NBitcoin;
 using BRhodium.Node.P2P;
+using BRhodium.Node.Utilities;
 
 namespace BRhodium.Bitcoin.Features.RPC.Controllers
 {
@@ -30,6 +31,9 @@ namespace BRhodium.Bitcoin.Features.RPC.Controllers
         /// </summary>
         private readonly ILogger logger;
 
+        /// <summary>Functionality of date and time.</summary>
+        private readonly IDateTimeProvider dateTimeProvider;
+
         /// <summary>
         /// The peer address manager
         /// </summary>
@@ -38,6 +42,7 @@ namespace BRhodium.Bitcoin.Features.RPC.Controllers
         public NetworkController(
             ILoggerFactory loggerFactory,
             IPeerAddressManager peerAddressManager,
+            IDateTimeProvider dateTimeProvider,
             IFullNode fullNode = null,
             NodeSettings nodeSettings = null,
             Network network = null,
@@ -54,6 +59,7 @@ namespace BRhodium.Bitcoin.Features.RPC.Controllers
         {
             this.logger = loggerFactory.CreateLogger(this.GetType().FullName);
             this.peerAddressManager = peerAddressManager;
+            this.dateTimeProvider = dateTimeProvider;
         }
 
         /// <summary>
@@ -61,9 +67,9 @@ namespace BRhodium.Bitcoin.Features.RPC.Controllers
         /// </summary>
         /// <param name="node">The node ip:port</param>
         /// <param name="command">'add' to add a node to the list, 'remove' to remove a node from the list, 'onetry' to try a connection to the node once</param>
-        /// <returns>True/False</returns>
-        [ActionName("getdifficulty")]
-        [ActionDescription("Result—the current difficulty.")]
+        /// <returns>True/Error</returns>
+        [ActionName("addnode")]
+        [ActionDescription("Attempts to add or remove a node from the addnode list. Or try a connection to a node once.")]
         public IActionResult AddNode(string node, string command)
         {
             try
@@ -171,7 +177,7 @@ namespace BRhodium.Bitcoin.Features.RPC.Controllers
         /// <summary>
         /// Clear all banned IPs.
         /// </summary>
-        /// <returns>True/False</returns>
+        /// <returns>True/Error</returns>
         [ActionName("clearbanned")]
         [ActionDescription("Result—the current difficulty.")]
         public IActionResult ClearBanned()
@@ -194,5 +200,99 @@ namespace BRhodium.Bitcoin.Features.RPC.Controllers
                 return ErrorHelpers.BuildErrorResponse(HttpStatusCode.BadRequest, e.Message, e.ToString());
             }
         }
+
+        /// <summary>
+        /// Immediately disconnects from the specified peer node.
+        /// 
+        /// Strictly one out of 'address' and 'nodeid' can be provided to identify the node.
+        ///
+        /// To disconnect by nodeid, either set 'address' to the empty string, or call using the named 'nodeid' argument only.
+        /// </summary>
+        /// <param name="address">The IP address:port of the node</param>
+        /// <param name="nodeId">The node ID (see getpeerinfo for node IDs)</param>
+        /// <returns>True/Error</returns>
+        [ActionName("disconnectnode")]
+        [ActionDescription("Immediately disconnects from the specified peer node.")]
+        public IActionResult DisconnectNode(string address, int? nodeId)
+        {
+            try
+            {
+                if ((string.IsNullOrEmpty(address)) && (!nodeId.HasValue))
+                {
+                    throw new ArgumentNullException("address");
+                }
+
+                if (!string.IsNullOrEmpty(address))
+                {
+                    var nodeParam = address.Split(':');
+                    if (nodeParam.Length < 2)
+                    {
+                        throw new ArgumentNullException("node");
+                    }
+
+                    var endPoint = new IPEndPoint(IPAddress.Parse(nodeParam[0]), int.Parse(nodeParam[1]));
+
+                    var node = this.ConnectionManager.FindNodeByEndpoint(endPoint);
+
+                    if (node != null)
+                    {
+                        node.Disconnect("RPC DisconnectNode call");
+                    }
+                }
+                else
+                {
+                    var peer = this.ConnectionManager.ConnectedPeers.ToList();
+                    
+                    if ((peer.Count > 0) && (peer.Count < nodeId.Value))
+                    {
+                        peer[nodeId.Value].Disconnect("RPC DisconnectNode call");
+                    }
+                }
+
+                return this.Json(ResultHelper.BuildResultResponse(true));
+            }
+            catch (Exception e)
+            {
+                this.logger.LogError("Exception occurred: {0}", e.ToString());
+                return ErrorHelpers.BuildErrorResponse(HttpStatusCode.BadRequest, e.Message, e.ToString());
+            }
+        }
+
+        /// <summary>
+        /// List all banned IPs/Subnets.
+        /// </summary>
+        /// <returns>True/Error</returns>
+        [ActionName("listbanned")]
+        [ActionDescription("List all banned IPs/Subnets.")]
+        public IActionResult ListBanned()
+        {
+            try
+            {
+                var bannedList = new List<string>();
+
+                var banned = this.peerAddressManager.Peers.Where(a => a.BanUntil.HasValue && a.BanUntil.Value > this.dateTimeProvider.GetUtcNow());
+
+                foreach (var item in banned)
+                {
+                    bannedList.Add(item.Endpoint.ToString());
+                }
+
+                return this.Json(ResultHelper.BuildResultResponse(bannedList));
+            }
+            catch (Exception e)
+            {
+                this.logger.LogError("Exception occurred: {0}", e.ToString());
+                return ErrorHelpers.BuildErrorResponse(HttpStatusCode.BadRequest, e.Message, e.ToString());
+            }
+        }
+
+        //getaddednodeinfo 
+        //getconnectioncount
+        //getnettotals
+        //getnetworkinfo
+        //ping
+        //setban
+        //setnetworkactive 
+
     }
 }
