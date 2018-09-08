b3868a3351afb65d630f97ba483b16273956ace1
BitCoin Rhodium <customers@bitcoinrh.org>
2018-09-08 10:30:29 +0200

    Addded based class for new RPCs

---
+++ src/BRhodium.Bitcoin.Features.Wallet/Controllers/UtilRPCController.cs
@@ -0,0 +1,36 @@
+﻿using BRhodium.Bitcoin.Features.Consensus.Interfaces;
+using BRhodium.Node;
+using BRhodium.Node.Base;
+using BRhodium.Node.Configuration;
+using BRhodium.Node.Connection;
+using BRhodium.Node.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NBitcoin;
+
+namespace BRhodium.Bitcoin.Features.Wallet.Controllers
+{
+    /// <summary>
+    /// RPC methods which support transaction functionality
+    /// </summary>
+    /// <seealso cref="BRhodium.Node.Controllers.FeatureController" />
+    [Controller]
+    public class UtilRPCController : FeatureController
+    {
+
+        private readonly ILoggerFactory loggerFactory;
+
+        public UtilRPCController(
+            ILoggerFactory loggerFactory,
+            IFullNode fullNode,
+            NodeSettings nodeSettings,
+            Network network,
+            ConcurrentChain chain,
+            IConnectionManager connectionManager,
+            IChainState chainState = null,
+            IConsensusLoop consensusLoop = null) : base(fullNode, nodeSettings, network, chain, chainState, connectionManager)
+        {
+            this.loggerFactory = loggerFactory;
+        }
+    }
+}
