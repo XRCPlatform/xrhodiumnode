1d2c9dc3bed95337d5ced783af0c599d204c80a5
DevRhodium2 <DevRhodium2>
2018-12-20 18:37:14 +0000

    Wallet notify feature
    Wallet notify feature

---
+++ src/BRhodium.Bitcoin.Features.Wallet/ShellHelper.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace BRhodium.Bitcoin.Features.Wallet
+{
+    public static class OS
+    {
+        public static bool IsWin() =>
+            RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+
+        public static bool IsMac() =>
+            RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+
+        public static bool IsGnu() =>
+            RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+
+        public static string GetCurrent()
+        {
+            return
+            (IsWin() ? "win" : null) ??
+            (IsMac() ? "mac" : null) ??
+            (IsGnu() ? "gnu" : null);
+        }
+    }
+
+    public class Response
+    {
+        public int code { get; set; }
+        public string stdout { get; set; }
+        public string stderr { get; set; }
+    }
+
+    public enum Output
+    {
+        Hidden,
+        Internal,
+        External
+    }
+
+    public static class ShellHelper
+    {
+        private static string GetFileName()
+        {
+            string fileName = "";
+            try
+            {
+                switch (OS.GetCurrent())
+                {
+                    case "win":
+                        fileName = "cmd.exe";
+                        break;
+                    case "mac":
+                    case "gnu":
+                        fileName = "/bin/bash";
+                        break;
+                }
+            }
+            catch (Exception Ex)
+            {
+                Console.WriteLine(Ex.Message);
+            }
+            return fileName;
+        }
+
+
+
+        public static Response Run(string cmd,  string dir = "")
+        {
+            var result = new Response();
+            var stderr = new StringBuilder();
+            var stdout = new StringBuilder();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = GetFileName();
+            startInfo.Arguments = cmd;
+            startInfo.RedirectStandardOutput = false;
+            startInfo.RedirectStandardError = false;
+            startInfo.UseShellExecute = true;
+            startInfo.CreateNoWindow = false;
+            if (!String.IsNullOrEmpty(dir))
+            {
+                startInfo.WorkingDirectory = dir;
+            }
+
+            Process process = Process.Start(startInfo);
+            //result.stdout = process.StandardOutput.ReadToEnd();
+            //result.stderr = process.StandardError.ReadToEnd();
+            process.WaitForExit();
+            return result;
+        }
+    }
+}
---
+++ src/BRhodium.Bitcoin.Features.Wallet/WalletNotification.cs
@@ -0,0 +1,39 @@
+﻿namespace BRhodium.Bitcoin.Features.Wallet
+{
+    public class WalletNotification
+    {
+        private TransactionNotificationType trigger;
+        private string command;
+
+        public WalletNotification(string subcription)
+        {
+            if (subcription.Contains(":") && (subcription.Contains("recieved") | subcription.Contains("sent")))
+            {
+                
+                string trigger = subcription.Substring(0, subcription.IndexOf(":"));
+                string command = subcription.Substring(subcription.IndexOf(":"));
+
+                switch (trigger.ToLower())
+                {
+                    case "recieved":
+                        this.trigger = TransactionNotificationType.Received;
+                        break;
+                    case "sent":
+                        this.trigger = TransactionNotificationType.Sent;
+                        break;
+                    default:
+                        break;
+                }
+                this.command = command;               
+            }
+            else
+            {
+                trigger = TransactionNotificationType.All;
+                this.command = subcription;
+            }
+        }
+
+        public TransactionNotificationType Trigger { get => this.trigger; set => this.trigger = value; }
+        public string Command { get => this.command; set => this.command = value; }
+    }
+}
