75ed8fa75607331111f2663e1e312848c1ab6bdb
BitCoin Rhodium <customers@bitcoinrh.org>
2019-02-28 19:26:29 +0100

    Work rule test improvement

--- src/BRhodium.Bitcoin.Features.Consensus.Tests/Rules/CommonRules/CalculateWorkRuleTest.cs
+++ src/BRhodium.Bitcoin.Features.Consensus.Tests/Rules/CommonRules/CalculateWorkRuleTest.cs
@@ -26,12 +26,14 @@ namespace BRhodium.Bitcoin.Features.Consensus.Tests.Rules.CommonRules
 
             await this.consensusRules.RegisterRule<CalculateWorkRule>().RunAsync(this.ruleContext);
 
-            Assert.Equal(0.465, this.ruleContext.NextWorkRequired.Difficulty);
+            Assert.Equal(0.000000000465, this.ruleContext.NextWorkRequired.Difficulty);
         }
 
         [Fact]
         public async Task RunAsync_ProofOfWorkBlock_CheckPow_InValidPow_ThrowsHighHashConsensusErrorExceptionAsync()
         {
+            this.network = Network.RegTest;
+            this.concurrentChain = MineChainWithHeight(2, this.network);
             this.ruleContext.BlockValidationContext = new BlockValidationContext()
             {
                 Block = new Block(new BlockHeader())
@@ -41,7 +43,7 @@ namespace BRhodium.Bitcoin.Features.Consensus.Tests.Rules.CommonRules
                             new NBitcoin.Transaction()
                         }
                 },
-                ChainedHeader = this.concurrentChain.GetBlock(4)
+                ChainedHeader = this.concurrentChain.GetBlock(2)
             };
             this.ruleContext.CheckPow = true;
 
