d98529f785744d8120400104318fc64c9cb1cf9f
rodendron <rodendron@xrhodium.org>
2021-08-24 11:26:57 +0100

    added mutisig scheme to support bip-45 multisig address deriviation

---
+++ src/BRhodium.Bitcoin.Features.Wallet/MultisigScheme.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BRhodium.Bitcoin.Features.Wallet
+{
+    public class MultisigScheme
+    {
+        /// <summary>
+        /// How many signatures will be suffient to move the funds.
+        /// </summary>
+        public int Threashold { get; set; }
+
+        /// <summary>
+        /// Cosigner extended pubkeys. Intentinaly not including any xPriv at this stage as such model is simplest to start with.
+        /// </summary>
+        public string[] XPubs { get; set; }
+    }
+}
